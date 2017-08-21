1d4c8892596a6d12f35ff0fcfb544349adc1cdfd
liyuming <yuming.li@intel.com>
2017-08-21 12:18:44 +0800

    s5

--- install-opencldriver4.1.sh
+++ install-opencldriver4.1.sh
@@ -5,7 +5,7 @@ sudo apt-get upgrade
 mkdir -p $HOME/opencl
 cd $HOME/opencl
 wget http://registrationcenter-download.intel.com/akdlm/irc_nas/11396/SRB4.1_linux64.zip
-unzip SRB5.0_linux64.zip
+unzip SRB4.1_linux64.zip
 mkdir -p ./intel-opencl
 tar -C intel-opencl -Jxf $HOME/opencl/intel-opencl-cpu-r4.1-61547.x86_64.tar.xz
 tar -C intel-opencl -Jxf $HOME/opencl/intel-opencl-devel-r4.1-61547.x86_64.tar.xz
