8aabcb35990390e6c5cd8c2c4178fd1343437966
liyuming <yuming.li@intel.com>
2017-09-04 10:04:21 +0800

    add windows installation

---
+++ Windows_install/nuget/nuget-create.cmd
@@ -0,0 +1,15 @@
+@echo off
+@setlocal EnableDelayedExpansion
+
+:: download https://dist.nuget.org/win-x86-commandline/latest/nuget.exe
+
+cd %~sdp0
+mkdir include
+mkdir lib
+cd lib
+mkdir Release
+cd %~sdp0
+xcopy %~sdp0\..\caffe\build\install\include %~sdp0\include /s /h /c /y 
+xcopy %~sdp0\..\caffe\build\install\lib %~sdp0\lib\Release /s /h /c /y 
+
+nuget.ext pack clCaffe_Package.nuspec
